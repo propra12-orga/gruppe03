b5f22e1ce5cc33718486a34ae995655733f512bf
Tobias Joosten <tobias.joosten@hhu.de>
2012-05-17 01:38:04 +0200

    bombe mit variablen und bild

--- src/bomber/Bombe.java
+++ src/bomber/Bombe.java
@@ -1,5 +1,16 @@
 package bomber;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 public class Bombe {
+	Image bomb;
+	int radius;
 	
+	public Bombe(int radius){
+		ImageIcon i = new ImageIcon("bomb.jpg");
+		bomb = i.getImage();
+		radius= this.radius;
+	}
 }
