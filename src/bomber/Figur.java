8eea7cc5ae5e9e2dc982078a8ac3425c97b07838
Tobias Joosten <tobias.joosten@hhu.de>
2012-05-17 00:58:21 +0200

    +package bomber figur rework

---
+++ src/bomber/Figur.java
@@ -0,0 +1,18 @@
+package bomber;
+
+public class Figur {
+	public int x, y,dx,dy;
+	public Figur(int xPosition,int yPosition){
+		x=xPosition;
+		y=yPosition;
+	}
+	
+	private int getX(){
+		return x;
+	}
+	private int getY(){
+		return y;
+	}
+	
+	
+}
