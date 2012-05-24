54da42ffb147cdc718e2ffeae0389ecf0b064c0a
Tobias Joosten <tobias.joosten@uni-duesseldorf.de>
2012-05-24 15:35:28 +0200

    bug fixx

--- src/bomber/Frame.java
+++ src/bomber/Frame.java
@@ -5,14 +5,14 @@ import javax.swing.JFrame;
 public class Frame {
 
 	public Frame(){
-		JFrame menu = new JFrame();
-		menu.add(new Board());
+/*		JFrame menu = new JFrame();
+		menu.add(new Menu());
 		menu.setTitle("our Bombing Game 2 gud");
 		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		menu.setSize(1000,700);
 		menu.setVisible(true);
 		menu.setLocationRelativeTo(null);
-		
+	*/	
 		JFrame frame = new JFrame();
 		frame.add(new Board());
 		frame.setTitle("our Bombing Game 2 gud");
