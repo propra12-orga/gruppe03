7533b320bcee0eaa77ec0f0b7aa4335c7b69e8eb
Tobias Joosten <tobias.joosten@uni-duesseldorf.de>
2012-05-24 13:32:57 +0200

    block code

--- src/bomber/Block.java
+++ src/bomber/Block.java
@@ -1,5 +1,28 @@
 package bomber;
 
-public class Block {
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
+public class Block {
+	
+		int x, y,dx,dy;
+		Image block;
+		
+		public Figur(int xPosition,int yPosition){
+			x=xPosition;
+			y=yPosition;
+			ImageIcon i = new ImageIcon("");
+			guy = i.getImage();
+		}
+		
+		public int getX(){
+			return x;
+		}
+		public int getY(){
+			return y;
+		}	
+		public Image getImage(){
+			return guy;
+		}
 }
