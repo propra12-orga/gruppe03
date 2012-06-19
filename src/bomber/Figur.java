8a147386a67073904caa938b143052907dd1ce1a
Tobias Joosten <tobias.joosten@uni-duesseldorf.de>
2012-06-20 00:54:05 +0200

    bogen teil 2

--- src/bomber/Figur.java
+++ src/bomber/Figur.java
@@ -5,30 +5,30 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Figur {
-	private int m, x, y, dx,dxl,dxr, dy,dyl,dyr, radi, maxBombs, bombsWorking, richtung, bild;
+	private int m, x, y, dx, dxl, dxr, dy, dyl, dyr, radi, maxBombs, bombsWorking, richtung, bild;
 	private Image forward, right, left, back, dead;
 	private boolean isAlive;
-	
-	public static final int oben=1;
-	public static final int unten=2;
-	public static final int rechts=3;
-	public static final int links=4;
-	public static final int startmovement=3;
-	
+
+	public static final int oben = 1;
+	public static final int unten = 2;
+	public static final int rechts = 3;
+	public static final int links = 4;
+	public static final int startmovement = 3;
+
 	public Figur(int xPosition, int yPosition, int Bild) {
 		m = startmovement; // movementreichweite
 		isAlive = true;
-		this.bild=Bild;
+		this.bild = Bild;
 		x = xPosition;
 		y = yPosition;
-		bombsWorking=0;
+		bombsWorking = 0;
 		maxBombs = 1;
 		radi = 2;
-		dxl=0;
-		dxr=0;
-		dyl=0;
-		dxl=0;
-		richtung=unten;
+		dxl = 0;
+		dxr = 0;
+		dyl = 0;
+		dxl = 0;
+		richtung = unten;
 		ImageIcon idead = new ImageIcon("bilder/schaedel.png");
 		dead = idead.getImage();
 
@@ -42,7 +42,7 @@ public class Figur {
 			ImageIcon i4 = new ImageIcon("Images/Player 1/Back1.jpg");
 			back = i4.getImage();
 		} else if (Bild == 2) {
-			//TODO 2. spieler richtugen
+			// TODO 2. spieler richtugen
 			ImageIcon i = new ImageIcon("bilder/huhn.png");
 			forward = i.getImage();
 		}
@@ -55,33 +55,39 @@ public class Figur {
 	public int getY() {
 		return y;
 	}
-	public int getMovementspeed(){
+
+	public int getMovementspeed() {
 		return m;
 	}
-	public void setMaxBombs(int maxB){
-		if (maxB>0)
-		maxBombs=maxB;
+
+	public void setMaxBombs(int maxB) {
+		if (maxB > 0)
+			maxBombs = maxB;
 	}
-	public void bombsWorkingPlus(){
-		if (bombsWorking<maxBombs)
-			bombsWorking+=1;
+
+	public void bombsWorkingPlus() {
+		if (bombsWorking < maxBombs)
+			bombsWorking += 1;
 	}
-	public void bombsWorkingMinus(){
-		if (bombsWorking>0)
-			bombsWorking-=1;
+
+	public void bombsWorkingMinus() {
+		if (bombsWorking > 0)
+			bombsWorking -= 1;
 	}
+
 	public Image getImage() {
 		if (isAlive == true) {
-			if (richtung==unten){
-				return forward;}
-			else if (richtung==rechts){
-				return right;}
-			else if (richtung==links){
-				return left;}
-			else if (richtung==oben){
-				return back;}
-			else{
-				return dead;}
+			if (richtung == unten) {
+				return forward;
+			} else if (richtung == rechts) {
+				return right;
+			} else if (richtung == links) {
+				return left;
+			} else if (richtung == oben) {
+				return back;
+			} else {
+				return dead;
+			}
 		} else {
 			return dead;
 		}
@@ -93,7 +99,7 @@ public class Figur {
 
 	// bombe legen
 	public void setBomb(Field feld) {
-		if (maxBombs > bombsWorking &&  feld.getArry(gethauptarrayX(), gethauptarrayY()) == 0 && isAlive) {
+		if (maxBombs > bombsWorking && feld.getArry(gethauptarrayX(), gethauptarrayY()) == 0 && isAlive) {
 			Thread bombe = new Thread(new Bombe(gethauptarrayX(), gethauptarrayY(), radi, feld, Figur.this));
 			bombe.start();
 		}
@@ -102,98 +108,125 @@ public class Figur {
 	// einfache Funktionen
 	public void moveLR() {
 		x += dx;
-		if (dx>0){
-			richtung=rechts;}
-		if (dx<0){
-			richtung=links;}
+		if (dx > 0) {
+			richtung = rechts;
+		}
+		if (dx < 0) {
+			richtung = links;
+		}
 	}
+
 	public void moveUD() {
 		y += dy;
-		if (dy<0)
-			richtung=oben;
-		if (dy>0)
-			richtung=unten;	
+		if (dy < 0)
+			richtung = oben;
+		if (dy > 0)
+			richtung = unten;
 	}
+
 	public void setdxl(int newdx) {
 		dxl = newdx;
 	}
+
 	public void setdxr(int newdx) {
 		dxr = newdx;
 	}
-	private void setdx(int newdx){
-		dx=newdx;
+
+	private void setdx(int newdx) {
+		dx = newdx;
 	}
-	private void setdy(int newdy){
-		dy=newdy;
+
+	private void setdy(int newdy) {
+		dy = newdy;
 	}
+
 	public void setdyl(int newdy) {
 		dyl = newdy;
 	}
+
 	public void setdyr(int newdy) {
 		dyr = newdy;
 	}
+
 	public int getdy() {
 		return dy;
 	}
+
 	public int getdx() {
 		return dx;
-	}	
+	}
+
 	// Hauptarray der Figur
 	public int gethauptarrayX() {
 		int posx = (x + 30) / 60;
 		return posx;
 	}
+
 	public int gethauptarrayY() {
 		int posy = (y + 30) / 60;
 		return posy;
 	}
+
 	public void restart(int xPos, int yPos) {
-		x=xPos;
-		y=yPos;
-		dx=0;
-		dy=0;
-		m = startmovement; 
+		x = xPos;
+		y = yPos;
+		dx = 0;
+		dy = 0;
+		m = startmovement;
 		isAlive = true;
-		bombsWorking=0;
+		bombsWorking = 0;
 		maxBombs = 1;
 		radi = 2;
-		dxl=0;
-		dxr=0;
-		dyl=0;
-		dxl=0;
-		richtung=unten;
-		
+		dxl = 0;
+		dxr = 0;
+		dyl = 0;
+		dxl = 0;
+		richtung = unten;
+
 	}
-//ITEM FUNKTIONEN
-	public void getBombentasche(){
-		maxBombs +=1;
+
+	// ITEM FUNKTIONEN
+	public void getBombentasche() {
+		maxBombs += 1;
 	}
-	public void getReichweite(){
-		radi +=1;
+
+	public void getReichweite() {
+		radi += 1;
 	}
-	public void getStiefel(){
-		if (m==3)
-			m=4;
-		if (m==4)
-			m=5;
+
+	public void getStiefel() {
+		if (m == 3)
+			m = 4;
+		if (m == 4)
+			m = 5;
 	}
+
+	private void getBogen() {
+		// TODO Pfeilschiessen konstruieren
+
+	}
+
 	// Smart Moving Vers 1.0
 	// Abfrage rechts links
 	public void Perma(Field feld) {
-		//itemabfrage
-		if (feld.isItem(gethauptarrayX(), gethauptarrayY())){
-			if (feld.getArry(gethauptarrayX(), gethauptarrayY())== Field.bombentasche){
+		// itemabfrage
+		if (feld.isItem(gethauptarrayX(), gethauptarrayY())) {
+			if (feld.getArry(gethauptarrayX(), gethauptarrayY()) == Field.bombentasche) {
 				getBombentasche();
 				feld.setArry(gethauptarrayX(), gethauptarrayY(), Field.block0);
-			}else if (feld.getArry(gethauptarrayX(), gethauptarrayY())== Field.reichweite){
+			} else if (feld.getArry(gethauptarrayX(), gethauptarrayY()) == Field.reichweite) {
 				getReichweite();
 				feld.setArry(gethauptarrayX(), gethauptarrayY(), Field.block0);
-			}else if (feld.getArry(gethauptarrayX(), gethauptarrayY())== Field.stiefel && x%60==0 &&y%60==0){
+			} else if (feld.getArry(gethauptarrayX(), gethauptarrayY()) == Field.stiefel && x % 60 == 0 && y % 60 == 0) {
 				getStiefel();
 				feld.setArry(gethauptarrayX(), gethauptarrayY(), Field.block0);
-		} }
-		dx=dxr+dxl;
-		dy=dyr+dyl;
+			} else if (feld.getArry(gethauptarrayX(), gethauptarrayY()) == Field.bogen) {
+				getBogen();
+				feld.setArry(gethauptarrayX(), gethauptarrayY(), Field.block0);
+			}
+		}
+		dx = dxr + dxl;
+		dy = dyr + dyl;
 		if (feld.kannManDraufLeben(gethauptarrayX(), gethauptarrayY()) && isAlive == true) {
 			if (dx != 0) {
 				if (y % 60 > 0 && y % 60 < 30) { // Zentralisierung
@@ -207,7 +240,7 @@ public class Figur {
 				} else if (y % 60 == 0) {
 					// Movement
 					if (dx > 0) {
-						if (feld.isWalkable(gethauptarrayX() + 1, gethauptarrayY()) ||x % 60 != 0) {
+						if (feld.isWalkable(gethauptarrayX() + 1, gethauptarrayY()) || x % 60 != 0) {
 							moveLR();
 						}
 					} else if (dx < 0) {
@@ -245,5 +278,4 @@ public class Figur {
 		}
 	}
 
-	
 }
