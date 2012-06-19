a84c84dc8ad62583368296156e0b5a653cc2ab47
Tobias Joosten <tobias.joosten@uni-duesseldorf.de>
2012-06-19 12:38:04 +0200

    menue erstellt: Exit funktioniert NEu noch inner mache

--- src/bomber/Frame.java
+++ src/bomber/Frame.java
@@ -1,28 +1,40 @@
 package bomber;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Frame {
 
-	public Frame(){
-/*		JFrame menu = new JFrame();
-		menu.add(new Menu());
-		menu.setTitle("our Bombing Game 2 gud");
-		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		menu.setSize(1000,700);
-		menu.setVisible(true);
-		menu.setLocationRelativeTo(null);
-	*/	
+	//public  Frame() {
+	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.add(new Board());
 		frame.setTitle("our Bombing Game 2 gud");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(60*15+100,60*11+46);
+		frame.setSize(60 * 15 + 100, 60 * 11 + 70);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
+
+		// MEnü
+		JMenuBar menubar = new JMenuBar();
+		frame.setJMenuBar(menubar);
+		JMenu menu = new JMenu("Game");
+		JMenuItem neu = new JMenuItem("Neues Spiel");
+		JMenuItem exit = new JMenuItem("Exit");
+		menubar.add(menu);
+		menu.add(neu);
+		menu.add(exit);
+
+		class exitAction implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		}
+		exit.addActionListener(new exitAction());
 	}
-	public static void main(String[] args){
-		new Frame();
-	}
+
+	//public static void main(String[] args) {
+		//new Frame();
+	//}
 
 }
