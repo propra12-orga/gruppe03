d754ab8c67554da398a9819b4d7ccd7d812dc3f5
Tobias Joosten <tobias.joosten@uni-duesseldorf.de>
2012-06-25 13:05:50 +0200

    level ladknöpfe im menu und levelordner mit dem ladbaren textdatein welche aber noch leer sind

--- src/bomber/Frame.java
+++ src/bomber/Frame.java
@@ -4,21 +4,31 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Frame {
-	public static boolean NeuesSpiel;
-
+	public static boolean NeuesSpiel, laden;
+	public static int leveltoload;
 	// public Frame() {
 	public Frame() {
+		leveltoload=1;
+		laden=false;
 		Board.running = false;
 		JFrame frame = new JFrame();
-		// MEnü
+		// Menü
 		JMenuBar menubar = new JMenuBar();
 		frame.setJMenuBar(menubar);
 		JMenu menu = new JMenu("Game");
+		JMenu laden = new JMenu("Level laden");
+		JMenuItem level1 = new JMenuItem("Level1");
+		JMenuItem level2 = new JMenuItem("Level2");
+		JMenuItem level3 = new JMenuItem("Level3");
 		JMenuItem neu = new JMenuItem("Neues Spiel");
 		JMenuItem exit = new JMenuItem("Exit    [ESC]");
 		menubar.add(menu);
 		menu.add(neu);
+		menu.add(laden);
 		menu.add(exit);
+		laden.add(level1);
+		laden.add(level2);
+		laden.add(level3);
 
 		frame.add(new Board());
 		frame.setTitle("our Bombing Game 2 gud");
@@ -27,8 +37,8 @@ public class Frame {
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 
-		//TODO Levelloading einbauen
-		
+		// TODO Levelloading einbauen
+
 		class neuAction implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
 				Board.winner = 0;
@@ -45,9 +55,30 @@ public class Frame {
 				System.exit(0);
 			}
 		}
-
+		class level1Action implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				Frame.laden=true;
+				Frame.leveltoload=1;
+			}
+		}
+		class level2Action implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				Frame.laden=true;
+				Frame.leveltoload=2;
+			}
+		}
+		class level3Action implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				Frame.laden=true;
+				Frame.leveltoload=3;
+			}
+		}
 		exit.addActionListener(new exitAction());
 		neu.addActionListener(new neuAction());
+		level1.addActionListener(new level1Action());
+		level2.addActionListener(new level2Action());
+		level3.addActionListener(new level3Action());
+
 	}
 
 	public static void main(String[] args) {
